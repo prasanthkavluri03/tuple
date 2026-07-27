c76d3713fe06e0d2c85c8fdc78d14a79c97b672b
kavluriprasanth03 <kavluriprasanth03@gmail.com>
2026-07-27 19:37:31 +0530

    first commit

--- 1.py
+++ 1.py
@@ -1,8 +1,8 @@
 # create a tuple with 5 student names
 
-tuple=("ravi","sai","satya","ram","srinu")
+tuple1=("ravi","sai","satya","ram","srinu")
 
-print(tuple) #('ravi', 'sai', 'satya', 'ram', 'srinu')
+print(tuple1) #('ravi', 'sai', 'satya', 'ram', 'srinu')
 
-s=type(tuple)
-print(s) #<class 'tuple'>
+s1=type(tuple1)
+print(s1) #<class 'tuple'>
