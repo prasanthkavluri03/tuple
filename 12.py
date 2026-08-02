02b9bc71045f8778d4dcfdcdb82a61b7492c617d
kavluriprasanth03 <kavluriprasanth03@gmail.com>
2026-08-02 22:07:53 +0530

    first commit

--- 12.py
+++ 12.py
@@ -1,6 +1,6 @@
 #print the last 3 elements of a tuple
 
-t1=(10,20,30,40,50,60,70,80,90,100)
+tuple=(10,20,30,40,50,60,70,80,90,100)
 
-s=t1[-3:]
-print(s)  #(80, 90, 100)
+sum=tuple[-3:]
+print(sum)  #(80, 90, 100)
