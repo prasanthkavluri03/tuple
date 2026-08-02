71cac21d07f024a66d71d6dad9d2b0dc7e67a347
kavluriprasanth03 <kavluriprasanth03@gmail.com>
2026-08-02 22:06:56 +0530

    first commit

--- 11.py
+++ 11.py
@@ -1,6 +1,6 @@
 # create a tuple of 10 numbers and print element from index 2 to index 6
 
-t1=(1,2,3,4,5,6,7,8,9,10)
+tuple=(1,2,3,4,5,6,7,8,9,10)
 
-i=t1[2:6]
-print(i) #(3, 4, 5, 6)
+val=tuple[2:6]
+print(val) #(3, 4, 5, 6)
