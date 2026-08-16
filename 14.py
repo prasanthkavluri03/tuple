f0d9bfe8bf274b6daa78242ca1f5037df2acad72
kavluriprasanth03 <kavluriprasanth03@gmail.com>
2026-08-16 19:56:22 +0530

    first commit

--- 14.py
+++ 14.py
@@ -1,5 +1,5 @@
 # print all elements except the last elements
 
-t1=(10,20,30,40,50,60,70,80,90,100)
-s1=t1[:-1]
-print(s1)  #(10, 20, 30, 40, 50, 60, 70, 80, 90)
+tuple=(10,20,30,40,50,60,70,80,90,100)
+value=tuple[:-1]
+print(value)  #(10, 20, 30, 40, 50, 60, 70, 80, 90)
