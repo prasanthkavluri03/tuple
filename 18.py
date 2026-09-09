62bb9e5db6af3fe3bbc3b441b6aad2966888f60b
kavluriprasanth03 <kavluriprasanth03@gmail.com>
2026-09-09 15:13:11 +0530

    new update

--- 18.py
+++ 18.py
@@ -1,5 +1,5 @@
 # print elements at odd index positions
 
-t1=(1,2,3,4,5,6,7,8,9,10)
-s1=t1[::2]
-print(s1)  #(1, 3, 5, 7, 9)
+tuple=(1,2,3,4,5,6,7,8,9,10)
+sum=tuple[::2]
+print(sum)  #(1, 3, 5, 7, 9)
