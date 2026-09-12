95ff29775211004fec14c6ebffeeb9e3fd185b7e
kavluriprasanth03 <kavluriprasanth03@gmail.com>
2026-09-12 22:54:59 +0530

    first commit

--- 25.py
+++ 25.py
@@ -1,5 +1,5 @@
 # check whether 'java' is present in a tuple
 
-t1=("python","java","sql","html")
-s1= "java" in (t1)
-print(s1)  #True
+tuple1=("python","java","sql","html")
+sum1= "java" in (tuple1)
+print(sum1)  #True
