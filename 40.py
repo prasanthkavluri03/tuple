1a329d5d53c9dff1f9037e7cc7260b0ba786e45c
kavluriprasanth03 <kavluriprasanth03@gmail.com>
2026-09-25 15:42:25 +0530

    first commit

--- 40.py
+++ 40.py
@@ -1,7 +1,7 @@
 #   Sort a tuple in ascending order. 
 
-t1=(1,3,5,6,8,9,10,2,4,7,11)
+tuple1=(1,3,5,6,8,9,10,2,4,7,11)
 
-s1=sorted(t1)
+sum1=sorted(tuple1)
 
-print(s1) #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+print(sum1) #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
