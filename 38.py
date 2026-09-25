75f39a88fa525cdfc100144201faca92d44bf28a
kavluriprasanth03 <kavluriprasanth03@gmail.com>
2026-09-25 15:41:47 +0530

    first commit

--- 38.py
+++ 38.py
@@ -1,7 +1,7 @@
 #  Find the sum of all values in a tuple.
 
-t1=(10,20,30,40,50,60,70,80,90,100)
+tuple1=(10,20,30,40,50,60,70,80,90,100)
 
-s1=sum(t1)
+sum1=sum(tuple1)
 
-print(s1)  #550
+print(sum1)  #550
